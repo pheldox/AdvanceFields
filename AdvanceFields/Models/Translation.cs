6b667c7d8f7013ffc6affd7ef9c1af223f73d8bd
felix <aryee@pces.mk>
2023-11-12 10:06:58 +0000

    Add project files.

---
+++ AdvanceFields/Models/Translation.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace AdvanceFields.Models
+{
+    public class Translation
+    {
+        public Success success;
+        public Content contents;
+    }
+
+    public class Content
+    {
+        public string translated { get; set; }
+        public string text { get; set; }
+        public string translation { get; set; }
+    }
+
+    public class Success
+    {
+        public int total { get; set; }
+    }
+}
---
+++ AdvanceFields/Startup.cs
@@ -0,0 +1,67 @@
+using jQuery_Ajax_CRUD.Models;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AdvanceFields
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddControllersWithViews();
+            services.AddDbContext<TransactionDbContext>(options =>
+         options.UseSqlServer(Configuration.GetConnectionString("DevConnection")));
+            var d = Configuration["TranslationSettings:BaseApiURL"];
+            services.AddHttpClient("Translation", (provider, client) =>
+            {
+             
+                client.BaseAddress = new Uri(Configuration["TranslationSettings:BaseApiURL"]);
+                TimeSpan.Parse(Configuration["TranslationSettings:Timeout"].ToString());
+            });
+        }
+
+        //Http client factory
+        
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+            else
+            {
+                app.UseExceptionHandler("/Home/Error");
+            }
+            app.UseStaticFiles();
+
+            app.UseRouting();
+
+            app.UseAuthorization();
+
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapControllerRoute(
+                    name: "default",
+                    pattern: "{controller=Translation}/{action=Index}/{id?}");
+            });
+        }
+    }
+}
